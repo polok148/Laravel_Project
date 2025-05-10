e41db34be08c800fd7db9a9d9e28705a7d9705e1
MD Shahidul Islam <polokmd833@gmail.com>
2025-05-11 00:15:13 +0600

    Home

--- SMS/routes/web.php
+++ SMS/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/home",[Controller::class,"Form"]);
